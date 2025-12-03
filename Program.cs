5a683bced149af0c0cdb7c3b2a6eb423f02552c2
ganmrizqan <ganmrizqan@gmail.com>
2025-12-03 22:58:36 +0700

    Tambah file tugas FizzBuzz

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Tampilkan "Fizz" jika habis dibagi 3
+// Tampilkan "Buzz" jika habis dibagi 5
+// Tampilkan "FizzBuzz" jika habis dibagi 3 dan 5
+
+Console.WriteLine("=== FizzBuzz Program (Tabel) ===\n");
+
+for (int i = 1; i <= 100; i += 5) 
+{
+    string row = "";
+    
+    for (int j = 0; j < 5 && (i + j) <= 100; j++) 
+    {
+        int num = i + j;
+        string result;
+        
+        if (num % 15 == 0) // Cek kelipatan 3 dan 5
+        {
+            result = "FizzBuzz";
+        }
+        else if (num % 3 == 0) // Cek kelipatan 3
+        {
+            result = "Fizz";
+        }
+        else if (num % 5 == 0) // Cek kelipatan 5
+        {
+            result = "Buzz";
+        }
+        else
+        {
+            result = num.ToString();
+        }
+        
+        row += $"{result.PadRight(10)} ";
+    }
+    
+    Console.WriteLine(row);
+}
